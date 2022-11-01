3a23f1cea70156479afd20ba1498beae78e210d2
Minh Nhat <nhat@example.com>
2022-11-01 22:51:41 +0700

    first commit

---
+++ Altest.py
@@ -0,0 +1,517 @@
+import os
+from matplotlib.pyplot import text
+import playsound
+import speech_recognition as sr
+from datetime import date
+import time
+import sys
+import ctypes
+import wikipedia
+import datetime
+from pynput.keyboard import Key, Controller
+import json
+import re
+import webbrowser
+import smtplib
+import requests
+import urllib
+import urllib.request as urllib2
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from time import strftime
+from gtts import gTTS
+from youtube_search import YoutubeSearch
+from threading import Thread
+import app
+from os.path import isfile, join
+import datetime
+import pyttsx3
+import Gesture_Controller
+
+
+
+
+
+# -------------Object Initialization---------------
+today = date.today()
+r = sr.Recognizer()
+keyboard = Controller()
+engine = pyttsx3.init('sapi5')
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+# ________________________________________________
+
+
+wikipedia.set_lang('vi')
+language = 'vi'
+path = ChromeDriverManager().install()
+
+def speak(text):
+    print("Bot: {}".format(text))
+    tts = gTTS(text=text, lang=language, slow=False)
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Tôi: ", end='')
+        audio = r.listen(source, phrase_time_limit=5)
+        try:
+            text = r.recognize_google(audio, language="vi-VN")
+            print(text)
+            return text
+        except:
+            print("...")
+            return 0
+
+def stop():
+    speak("Hẹn gặp lại bạn sau!")
+
+def get_text():
+    for i in range(3):
+        text = get_audio()
+        if text:
+            return text.lower()
+        elif i < 2:
+            speak("Bot không nghe rõ. Bạn nói lại được không!")
+    time.sleep(2)
+    stop()
+    return 0
+
+def wish():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour>=0 and hour<12:
+        reply("Good Morning!")
+    elif hour>=12 and hour<18:
+        reply("Good Afternoon!")   
+    else:
+        reply("Good Evening!")  
+        
+    reply("I am Proton, how may I help you?")
+
+def bye(text):
+    if "bye" in text:
+        speak('Hẹn gặp lại bạn sau nhé, hãy nhớ quay lại và thăm tôi')
+    else:
+        speak("Bot chưa hiểu ý của bạn. Bạn nói lại được không?")
+
+
+
+def get_time(text):
+    now = datetime.datetime.now()
+    if "giờ" in text:
+        speak('Bây giờ là %d giờ %d phút' % (now.hour, now.minute))
+    elif "ngày" in text:
+        speak("Hôm nay là ngày %d tháng %d năm %d" %
+              (now.day, now.month, now.year))
+    else:
+        speak("Bot chưa hiểu ý của bạn. Bạn nói lại được không?")
+
+
+def open_application(text):
+    if "google" in text:
+        speak("Mở Google Chrome")
+        os.startfile(
+            'C:\Program Files\Google\Chrome\Application')
+    elif "word" in text:
+        speak("Mở Microsoft Word")
+        os.startfile(
+            'C:\Program Files\Microsoft Office\\root\Office16\\WINWORD.EXE')
+    elif "excel" in text:
+        speak("Mở Microsoft Excel")
+        os.startfile(
+            'C:\Program Files\Microsoft Office\\root\Office16\EXCEL.EXE')
+    else:
+        speak("Ứng dụng chưa được cài đặt. Bạn hãy thử lại!")
+
+def open_website(text):
+    reg_ex = re.search('mở (.+)', text)
+    if reg_ex:
+        domain = reg_ex.group(1)
+        url = 'https://www.' + domain
+        webbrowser.open(url)
+        speak("Trang web bạn yêu cầu đã được mở.")
+        return True
+    else:
+        return False
+
+def open_google_and_search(text):
+    search_for = text.split("kiếm", 1)[1]
+    speak('Okay!')
+    driver = webdriver.Chrome(path)
+    driver.get("https://www.google.com")
+    que = driver.find_element_by_xpath("//input[@name='q']")
+    que.send_keys(str(search_for))
+    que.send_keys(Keys.RETURN)
+
+def send_email(text):
+    speak('Bạn gửi email cho ai nhỉ')
+    recipient = get_text()
+    if 'yến' in recipient:
+        speak('Nội dung bạn muốn gửi là gì')
+        content = get_text()
+        mail = smtplib.SMTP('smtp.gmail.com', 587)
+        mail.ehlo()
+        mail.starttls()
+        mail.login('luongngochungcntt@gmail.com', 'hung23081997')
+        mail.sendmail('luongngochungcntt@gmail.com',
+                      'hungdhv97@gmail.com', content.encode('utf-8'))
+        mail.close()
+        speak('Email của bạn vùa được gửi. Bạn check lại email nhé hihi.')
+    else:
+        speak('Bot không hiểu bạn muốn gửi email cho ai. Bạn nói lại được không?')
+
+def current_weather():
+    speak("Bạn muốn xem thời tiết ở đâu ạ.")
+    ow_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city = get_text()
+    if not city:
+        pass
+    api_key = "fe8d8c65cf345889139d8e545f57819a"
+    call_url = ow_url + "appid=" + api_key + "&q=" + city + "&units=metric"
+    response = requests.get(call_url)
+    data = response.json()
+    if data["cod"] != "404":
+        city_res = data["main"]
+        current_temperature = city_res["temp"]
+        current_pressure = city_res["pressure"]
+        current_humidity = city_res["humidity"]
+        suntime = data["sys"]
+        sunrise = datetime.datetime.fromtimestamp(suntime["sunrise"])
+        sunset = datetime.datetime.fromtimestamp(suntime["sunset"])
+        wthr = data["weather"]
+        weather_description = wthr[0]["description"]
+        now = datetime.datetime.now()
+        content = """
+        Hôm nay là ngày {day} tháng {month} năm {year}
+        Mặt trời mọc vào {hourrise} giờ {minrise} phút
+        Mặt trời lặn vào {hourset} giờ {minset} phút
+        Nhiệt độ trung bình là {temp} độ C
+        Áp suất không khí là {pressure} héc tơ Pascal
+        Độ ẩm là {humidity}%
+        Trời hôm nay quang mây. Dự báo mưa rải rác ở một số nơi.""".format(day = now.day,month = now.month, year= now.year, hourrise = sunrise.hour, minrise = sunrise.minute,
+                                                                           hourset = sunset.hour, minset = sunset.minute, 
+                                                                           temp = current_temperature, pressure = current_pressure, humidity = current_humidity)
+        speak(content)
+        time.sleep(20)
+    else:
+        speak("Không tìm thấy địa chỉ của bạn")
+
+def play_song():
+    speak('Xin mời bạn chọn tên bài hát')
+    mysong = get_text()
+    while True:
+        result = YoutubeSearch(mysong, max_results=10).to_dict()
+        if result:
+            break
+    url = 'https://www.youtube.com' + result[0]['url_suffix']
+    webbrowser.open(url)
+    speak("Bài hát bạn yêu cầu đã được mở.")
+
+def change_wallpaper():
+    api_key = 'RF3LyUUIyogjCpQwlf-zjzCf1JdvRwb--SLV6iCzOxw'
+    url = 'https://api.unsplash.com/photos/random?client_id=' + \
+        api_key  # pic from unspalsh.com
+    f = urllib2.urlopen(url)
+    json_string = f.read()
+    f.close()
+    parsed_json = json.loads(json_string)
+    photo = parsed_json['urls']['full']
+    # Location where we download the image to.
+    urllib2.urlretrieve(photo, "C:/Users/Night Fury/Downloads/a.png")
+    image=os.path.join("C:/Users/Night Fury/Downloads/a.png")
+    ctypes.windll.user32.SystemParametersInfoW(20,0,image,3)
+    speak('Hình nền máy tính vừa được thay đổi')
+
+def read_news():
+    speak("Bạn muốn đọc báo về gì")
+    
+    queue = get_text()
+    params = {
+        'apiKey': '30d02d187f7140faacf9ccd27a1441ad',
+        "q": queue,
+    }
+    api_result = requests.get('http://newsapi.org/v2/top-headlines?', params)
+    api_response = api_result.json()
+    print("Tin tức")
+
+    for number, result in enumerate(api_response['articles'], start=1):
+        print(f"""Tin {number}:\nTiêu đề: {result['title']}\nTrích dẫn: {result['description']}\nLink: {result['url']}
+    """)
+        if number <= 3:
+            webbrowser.open(result['url'])
+
+def tell_me_about():
+    try:
+        speak("Bạn muốn nghe về gì ạ")
+        text = get_text()
+        contents = wikipedia.summary(text).split('\n')
+        speak(contents[0])
+        time.sleep(10)
+        for content in contents[1:]:
+            speak("Bạn muốn nghe thêm không")
+            ans = get_text()
+            if "có" not in ans:
+                break    
+            speak(content)
+            time.sleep(10)
+
+        speak('Cảm ơn bạn đã lắng nghe!!!')
+    except:
+        speak("Bot không định nghĩa được thuật ngữ của bạn. Xin mời bạn nói lại")
+
+def help_me():
+    speak("""Bot có thể giúp bạn thực hiện các câu lệnh sau đây:
+    1. Chào hỏi
+    2. Hiển thị giờ
+    3. Mở website, application
+    4. Tìm kiếm trên Google
+    5. Gửi email
+    6. Dự báo thời tiết
+    7. Mở video nhạc
+    8. Thay đổi hình nền máy tính
+    9. Đọc báo hôm nay
+    10. Kể bạn biết về thế giới """)
+    time.sleep(20)
+
+def assistant():
+    speak("Xin chào, bạn tên là gì nhỉ?")
+#     name = get_text()
+    name = "Trần Đình Nhật"
+    if name:
+        speak("Chào bạn {}".format(name))
+        speak("Bạn cần Bot Alex có thể giúp gì ạ?")
+        while True:
+            text = get_text()
+            if not text:
+                break
+            elif "dừng" in text or "tạm biệt" in text or "chào robot" in text or "ngủ thôi" in text:
+                stop()
+                break
+            elif "có thể làm gì" in text:
+                help_me()
+            elif "chào trợ lý ảo" in text:
+                wish(name)
+            elif "hiện tại" in text:
+                get_time(text)
+            elif "mở" in text:
+                if 'mở google và tìm kiếm' in text:
+                    open_google_and_search(text)
+                elif "." in text:
+                    open_website(text)
+                else:
+                    open_application(text)
+            elif "email" in text or "mail" in text or "gmail" in text:
+                send_email(text)
+            elif "thời tiết" in text:
+                current_weather()
+            elif "chơi nhạc" in text:
+                play_song()
+            elif "hình nền" in text:
+                change_wallpaper()
+            elif "đọc báo" in text:
+                read_news()
+            elif "định nghĩa" in text:
+                tell_me_about()
+            elif 'bye' in text:
+                bye(text)
+            else:
+                speak("Bạn cần Bot giúp gì ạ?")
+
+                
+
+
+
+
+def wish():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour>=0 and hour<12:
+        reply("Good Morning!")
+    elif hour>=12 and hour<18:
+        reply("Good Afternoon!")   
+    else:
+        reply("Good Evening!")  
+        
+    reply("I am Proton, how may I help you?")
+
+
+
+
+def respond(voice_data):
+    global file_exp_status, files, is_awake, path
+    print(voice_data)
+    voice_data.replace('proton','')
+    app.eel.addUserMsg(voice_data)
+
+    if is_awake==False:
+        if 'wake up' in voice_data:
+            is_awake = True
+            wish()
+
+    # STATIC CONTROLS
+    elif 'hello' in voice_data:
+        wish()
+
+    elif 'what is your name' in voice_data:
+        reply('My name is Proton!')
+
+    elif 'date' in voice_data:
+        reply(today.strftime("%B %d, %Y"))
+
+    elif 'time' in voice_data:
+        reply(str(datetime.datetime.now()).split(" ")[1].split('.')[0])
+
+    elif 'search' in voice_data:
+        reply('Searching for ' + voice_data.split('search')[1])
+        url = 'https://google.com/search?q=' + voice_data.split('search')[1]
+        try:
+            webbrowser.get().open(url)
+            reply('This is what I found Sir')
+        except:
+            reply('Please check your Internet')
+
+    elif 'location' in voice_data:
+        reply('Which place are you looking for ?')
+        temp_audio = get_audio()
+        app.eel.addUserMsg(temp_audio)
+        reply('Locating...')
+        url = 'https://google.nl/maps/place/' + temp_audio + '/&amp;'
+        try:
+            webbrowser.get().open(url)
+            reply('This is what I found Sir')
+        except:
+            reply('Please check your Internet')
+
+    elif ('bye' in voice_data) or ('by' in voice_data):
+        reply("Good bye Sir! Have a nice day.")
+        is_awake = False
+
+    elif ('exit' in voice_data) or ('terminate' in voice_data):
+        if Gesture_Controller.GestureController.gc_mode:
+            Gesture_Controller.GestureController.gc_mode = 0
+        app.ChatBot.close()
+        #sys.exit() always raises SystemExit, Handle it in main loop
+        sys.exit()
+        
+    
+    # DYNAMIC CONTROLS
+    elif 'launch gesture recognition' in voice_data:
+        if Gesture_Controller.GestureController.gc_mode:
+            reply('Gesture recognition is already active')
+        else:
+            gc = Gesture_Controller.GestureController()
+            t = Thread(target = gc.start)
+            t.start()
+            reply('Launched Successfully')
+
+    elif ('stop gesture recognition' in voice_data) or ('top gesture recognition' in voice_data):
+        if Gesture_Controller.GestureController.gc_mode:
+            Gesture_Controller.GestureController.gc_mode = 0
+            reply('Gesture recognition stopped')
+        else:
+            reply('Gesture recognition is already inactive')
+        
+    elif 'copy' in voice_data:
+        with keyboard.pressed(Key.ctrl):
+            keyboard.press('c')
+            keyboard.release('c')
+        reply('Copied')
+          
+    elif 'page' in voice_data or 'pest'  in voice_data or 'paste' in voice_data:
+        with keyboard.pressed(Key.ctrl):
+            keyboard.press('v')
+            keyboard.release('v')
+        reply('Pasted')
+        
+    # File Navigation (Default Folder set to C://)
+    elif 'list' in voice_data:
+        counter = 0
+        path = 'C://'
+        files = listdir(path)
+        filestr = ""
+        for f in files:
+            counter+=1
+            print(str(counter) + ':  ' + f)
+            filestr += str(counter) + ':  ' + f + '<br>'
+        file_exp_status = True
+        reply('These are the files in your root directory')
+        app.ChatBot.addAppMsg(filestr)
+        
+    elif file_exp_status == True:
+        counter = 0   
+        if 'open' in voice_data:
+            if isfile(join(path,files[int(voice_data.split(' ')[-1])-1])):
+                os.startfile(path + files[int(voice_data.split(' ')[-1])-1])
+                file_exp_status = False
+            else:
+                try:
+                    path = path + files[int(voice_data.split(' ')[-1])-1] + '//'
+                    files = listdir(path)
+                    filestr = ""
+                    for f in files:
+                        counter+=1
+                        filestr += str(counter) + ':  ' + f + '<br>'
+                        print(str(counter) + ':  ' + f)
+                    reply('Opened Successfully')
+                    app.ChatBot.addAppMsg(filestr)
+                    
+                except:
+                    reply('You do not have permission to access this folder')
+                                    
+        if 'back' in voice_data:
+            filestr = ""
+            if path == 'C://':
+                reply('Sorry, this is the root directory')
+            else:
+                a = path.split('//')[:-2]
+                path = '//'.join(a)
+                path += '//'
+                files = listdir(path)
+                for f in files:
+                    counter+=1
+                    filestr += str(counter) + ':  ' + f + '<br>'
+                    print(str(counter) + ':  ' + f)
+                reply('ok')
+                app.ChatBot.addAppMsg(filestr)
+                   
+    else: 
+        reply('I am not functioned to do this !')
+
+
+
+
+
+def reply(audio):
+    app.ChatBot.addAppMsg(audio)
+
+    print(audio)
+    engine.say(audio)
+    engine.runAndWait()
+
+t1 = Thread(target = app.ChatBot.start)
+t1.start()
+
+# Lock main thread until Chatbot has started
+while not app.ChatBot.started:
+    time.sleep(0.5)
+
+wish()
+voice_data = None
+while True:
+    if app.ChatBot.isUserInput():
+        #take input from GUI
+        voice_data = app.ChatBot.popUserInput()
+    else:
+        #take input from Voice
+        voice_data = get_audio()
+
+    #process voice_data
+
+
+
+assistant()
